ad0162e3384ab91605fcd00c1c8cc182e2dbaca4
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-14 22:48:01 +0100

    gutenberg books component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
-import { useEffect, useState } from 'react';
 import { DictionarySearch } from './components/DictionarySearch';
+import { GutenbergBookSearch } from './components/GutenbergBookSearch';
 import './App.scss';
 
 function App() {
-	const [booksArray, setBooksArray] = useState([]);
-
-	useEffect(() => {
-		const url2 = 'https://gutendex.com/books/?search=colorado';
-		(async () => {
-			const response = await fetch(url2);
-			const data = await response.json();
-			console.log(data);
-			setBooksArray([...data.results]);
-		})();
-	}, []);
 
 	return (
 		<div className="App">
-			<DictionarySearch/>
-
-			<h2>Gutenberg Books</h2>
-			<ul>
-				{booksArray.map((book, i) => {
-					return (
-						<li key={i}><a target="_blank" href={book.formats['text/html']} rel="noreferrer">{book.title}</a> - {book.authors[0].name}</li>
-					)
-				})}
-			</ul>
+			<DictionarySearch />
+			<GutenbergBookSearch />
 		</div>
 	);
 }
---
+++ src/components/GutenbergBookSearch.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+export const GutenbergBookSearch = () => {
+	const [booksArray, setBooksArray] = useState([]);
+
+	useEffect(() => {
+		const url2 = 'https://gutendex.com/books/?search=colorado';
+		(async () => {
+			const response = await fetch(url2);
+			const data = await response.json();
+			console.log(data);
+			setBooksArray([...data.results]);
+		})();
+	}, []);
+
+
+	return (
+		<>
+			<h2>Gutenberg Books</h2>
+			<ul>
+				{booksArray.map((book, i) => {
+					return (
+						<li key={i}><a target="_blank" href={book.formats['text/html']} rel="noreferrer">{book.title}</a> - {book.authors[0].name}</li>
+					)
+				})}
+			</ul>
+		</>
+	)
+}
